cab6051bbb6da11be28801784f9ca1ff16c96700
Guenter Hipler <guenter.hipler@unibas.ch>
2017-07-30 17:52:39 +0200

    we need different methods for the preprocessing  of the documents in the Mongo DB because they are not normalized by contentCollector after fetching from the datasources

--- src/main/java/org/swissbib/mf/mongo/preprocessing/DefaultPreprocessing.java
+++ src/main/java/org/swissbib/mf/mongo/preprocessing/DefaultPreprocessing.java
@@ -1,36 +1,58 @@
 package org.swissbib.mf.mongo.preprocessing;
 
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DefaultPreprocessing {
 
-    private final static Pattern pRecord;
-    private final static Pattern pRecordRoot;
+    //private final static Pattern pRecord;
+    //private final static Pattern pRecordRoot;
     private final static String replacement;
-    static {
-        pRecord = Pattern.compile(".*?(<marc:record.*?</marc:record>).*?</metadata></record>",
-                Pattern.MULTILINE |
-                Pattern.CASE_INSENSITIVE |
-                Pattern.DOTALL);
 
-        pRecordRoot = Pattern.compile("<marc:record.*?>",
-                Pattern.MULTILINE |
-                        Pattern.CASE_INSENSITIVE |
-                        Pattern.DOTALL);
+    private final static HashMap<String, MatcherCriteria> matchersList;
+
+    static {
+
+        matchersList = new HashMap<>();
+        matchersList.put("DEFAULT",new MatcherCriteria(
+                Pattern.compile(".*?(<marc:record.*?</marc:record>).*?</metadata></record>",
+                        Pattern.MULTILINE |
+                                Pattern.CASE_INSENSITIVE |
+                                Pattern.DOTALL),
+                Pattern.compile("<marc:record.*?>",
+                        Pattern.MULTILINE |
+                                Pattern.CASE_INSENSITIVE |
+                                Pattern.DOTALL)
+        ) );
+
+
+        matchersList.put("SNL",new MatcherCriteria(
+                Pattern.compile(".*?(<marc:record>.*?</marc:record>).*?",
+                        Pattern.MULTILINE |
+                                Pattern.CASE_INSENSITIVE |
+                                Pattern.DOTALL),
+                Pattern.compile("<marc:record.*?>",
+                        Pattern.MULTILINE |
+                                Pattern.CASE_INSENSITIVE |
+                                Pattern.DOTALL)
+        ) );
+
+
+
         replacement = "<marc:record type=\"Bibliographic\" xmlns:marc=\"http://www.loc.gov/MARC21/slim\">";
 
     }
 
 
-    public static String cleanRecord(String dbRecord)
+    public static String cleanRecord(String dbRecord, String matcher)
     {
 
-        Matcher m  = pRecord.matcher(dbRecord);
+        Matcher m  = matchersList.get(matcher).getpRecord().matcher(dbRecord);
         String recordBody = "";
         if (m.matches())
             recordBody = m.group(1);
-            m = pRecordRoot.matcher(recordBody);
+            m =  matchersList.get(matcher).getpRecordRoot().matcher(recordBody);
             recordBody = m.replaceFirst(replacement);
 
 
@@ -39,6 +61,24 @@ public class DefaultPreprocessing {
 
     }
 
+    private static class MatcherCriteria {
 
+
+        Pattern pRecord;
+        Pattern pRecordRoot;
+
+        MatcherCriteria(Pattern pRecord, Pattern pRecordRoot) {
+            this.pRecord = pRecord;
+            this.pRecordRoot = pRecordRoot;
+        }
+
+        public Pattern getpRecord() {
+            return pRecord;
+        }
+
+        public Pattern getpRecordRoot() {
+            return pRecordRoot;
+        }
+    }
 
 }
